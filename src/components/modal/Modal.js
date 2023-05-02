d5c42a77b7353c8f250ec2e52611221f14e18745
nazmiyegnydnn <nazmiyeyilmaz159@gmail.com>
2023-05-02 14:15:23 +0300

    When the playlist modal is empty, the modal is closed.

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import "./Modal.scss"
 import { useSelector ,useDispatch} from 'react-redux';
 import {deleteFilter } from "../../dataSlice";
@@ -9,11 +9,18 @@ const Modal = ({setModal}) => {
     const closeClick = () =>{
       setModal(false)
     }
+
+    useEffect(() => {
+      if (addList.length === 0) {
+        setModal(false);
+      }
+    }, [addList]);
+    
   return (
     <div className='modal'>
         <h2>Çalma Listesi</h2>
-    { 
-        addList.map((el) =>
+    {
+       addList.map((el) =>
        <div className='playList'>
            <p className='playListText'>{el.name}</p>
            <button className='playListClose'  onClick={() => dispatch(deleteFilter(el.id))}>X</button>
